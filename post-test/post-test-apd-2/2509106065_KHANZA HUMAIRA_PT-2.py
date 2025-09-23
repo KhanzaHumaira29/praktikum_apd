3710e6769c12e6cddb5cee19def1318e4912ad81
KhanzaHumaira29 <khanzahumaira824@gmail.com>
2025-09-23 12:45:46 +0800

    tugaspts2

--- post-test/post-test-apd-2/2509106065_KHANZA HUMAIRA_PT-2.py
+++ post-test/post-test-apd-2/2509106065_KHANZA HUMAIRA_PT-2.py
@@ -1,27 +1,18 @@
-suhu = [0] * (6)
+#Data suhu dalam celcius
+suhu = [27, 33, 46, 55, 67, 92]
 
-print ("Masukkan suhu ke-1 dalam Celcius:")
-suhu [0] = int (input())
-print ("Masukkan suhu ke-2 dalam Celcius:")
-suhu [1] = int (input())
-print ("Masukkan suhu ke-3 dalam Celcius:")
-suhu [2] = int (input())
-print ("Masukkan suhu ke-4 dalam Celcius:")
-suhu [3] = int (input())
-print ("Masukkan suhu ke-5 dalam Celcius:")
-suhu [4] = int (input())
-print ("Masukkan suhu ke-6 dalam Celcius:")
-suhu [5] = int (input())
-fah1 = float (suhu[0]*9)/5+32
+#Konversi suhu ke dalam fahrenheit, kelvin, reamur
+fah1 = float (suhu[0]*9)/5+32   
 fah2 = float (suhu[1]*9)/5+32
 kel1 = float (suhu[2])+273.15
 kel2 = float (suhu[3])+273.15
 rea1 = float (suhu[4]*4)/5
 rea2 = float (suhu[5]*4)/5
+
+#Menghitung jumlah dan rata-rata suhu setelah di konversi
 jumlah = fah1 + fah2 + kel1 + kel2 + rea1 + rea2
 ratarata= jumlah / 6
-print ("Masukkan dua digit NIM terakhir Anda:")
-nIM = int (input())
+nIM = 65
 bolean = nIM < ratarata
 print ("Suhu ke-1=" + str (suhu [0]))
 print ("Suhu ke-2=" + str (suhu [1]))
@@ -29,17 +20,16 @@ print ("Suhu ke-3=" + str (suhu [2]))
 print ("Suhu ke-4=" + str (suhu [3]))
 print ("Suhu ke-5=" + str (suhu [4]))
 print ("Suhu ke-6=" + str (suhu [5]))
-print ("Fahrenheit1=" + str (fah1))
-print ("Fahrenheit2=" + str (fah2))
-print ("Kelvin1=" + str (kel1))
-print ("Kelvin2=" + str (kel2))
-print ("Reamur1=" + str (rea1))
-print ("Reamur2=" + str (rea2))
-print ("Jumlah=" + str (jumlah))
+print ("Fahrenheit dari suhu 27 derajat celcius=" + str (fah1),"F")
+print ("Fahrenheit dari suhu 33 derajat celcius=" + str (fah2), "F")
+print ("Kelvin dari suhu 46 derajat celcius=" + str (kel1), "K")
+print ("Kelvin dari suhu 55 derajat celcius=" + str (kel2), "K")
+print ("Reamur dari suhu 67 derajat celcius=" + str (rea1), "R")
+print ("Reamur dari suhu 92 derajat celcius=" + str (rea2), "R")
+print ("Jumlah suhu setelah di konversi=" + str (jumlah))
 print ("Rata-rata=" + str (ratarata))
 print ("NIM Anda=" + str (nIM))
 print ("Bolean:" + str (bolean))
-print ("Slice suhu ke-3=" + str (suhu[2]))
-print ("Slice suhu ke-4=" + str (suhu[3]))
-print ("Slice suhu ke-5=" + str (suhu[4]))
-print ("Slice suhu ke-6=" + str (suhu[5]))
+
+#Poin Plus
+print ("Berikut list suhu dari 46 sampai 92 (celcius):", suhu [-4:6])
